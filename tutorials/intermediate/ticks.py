13ac1aed375c7ef1679c7fe5567e17a67107253b
Jody Klymak <jklymak@gmail.com>
2018-03-26 17:54:16 -0700

    DOC

--- tutorials/intermediate/ticks.py
+++ tutorials/intermediate/ticks.py
@@ -16,6 +16,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.ticker as mticker
+import matplotlib.gridspec as gridspec
 
 mpl.rcParams['figure.constrained_layout.use'] = True
 mpl.rcParams['figure.figsize'] = 6.4, 3.5
@@ -43,8 +44,74 @@ axs[2].set_xlabel('Manual x-ticks and labels')
 plt.show()
 
 ############################################################################
+# Specifying minor ticks and labels
+# =================================
+#
+# We can also specify minor ticks.  We can do it manually, or automatically
+# using a `.ticker.Locator`.  Here we show both the `AutoMinorLocator` and
+# the `MultipleLocator`.
+
+fig, axs = plt.subplots(1, 3, figsize=(6.4, 2.5))
+axs[0].plot(np.arange(10))
+axs[0].xaxis.set_ticks(np.arange(0, 10, 1.25), minor=True)
+axs[0].set_xlabel('Manual minor x-ticks')
+
+minorLocator = mticker.AutoMinorLocator()
+
+axs[1].plot(np.arange(10))
+axs[1].xaxis.set_minor_locator(minorLocator)
+axs[1].set_xlabel('Auto minor x-ticks')
+
+multipleLocator = mticker.MultipleLocator(0.5)
+
+axs[2].plot(np.arange(10))
+axs[2].xaxis.set_minor_locator(multipleLocator)
+axs[2].set_xlabel('Set Multiple x-ticks')
+
+plt.show()
+
+############################################################################
+# Note that if we resize the Axes, the `.ticker.AutoMinorLocator`
+# adjusts the number of minor ticks. Automatic
+# locators are usually a better choice if the axes size is not known or
+# likely to change.
+
+fig = plt.figure(figsize=(6.4, 2.5))
+gs = gridspec.GridSpec(1, 10, figure=fig)
+ax = fig.add_subplot(gs[0])
+ax.plot(np.arange(10))
+minorLocator = mticker.AutoMinorLocator()
+ax.xaxis.set_minor_locator(minorLocator)
+ax.set_xlabel('Auto minor x-ticks')
+
+ax = fig.add_subplot(gs[1:])
+ax.plot(np.arange(10))
+minorLocator = mticker.AutoMinorLocator()
+ax.xaxis.set_minor_locator(minorLocator)
+ax.set_xlabel('Auto minor x-ticks')
+plt.show()
+
+############################################################################
+# Of course, an array of `.ticker.Locator` subclasses are also appropriate
+# for the major ticks as well.
+
+locators = [mticker.AutoLocator(), mticker.LogLocator(),
+        mticker.NullLocator()]
+label = ['Auto', 'Log', 'Null']
+fig, axs = plt.subplots(1, 3, figsize=(6.4, 2.5))
+for n, locator in enumerate(locators):
+    axs[n].plot(np.arange(1, 200))
+    axs[n].xaxis.set_major_locator(locator)
+    axs[n].set_xlabel(label[n])
+fig.align_xlabels(axs)
+plt.show()
+
+
+
+
+############################################################################
 # TODO:
-#   - minor ticks
+#   x minor ticks
 #   - Changing auto formatting...
 #      - options...
 #      - FuncFormatter...
