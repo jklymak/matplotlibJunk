d6557ad0d8bd8bc741993d1f2fd57ac41ccbaf54
Jody Klymak <jklymak@gmail.com>
2019-08-08 18:42:46 -0700

    DOC fix

--- lib/matplotlib/dates.py
+++ lib/matplotlib/dates.py
@@ -235,7 +235,8 @@ def set_epoch(epoch):
     precision, so an epoch within 50 days of the dates being considered
     can reach 1 nanosecond resolution.
 
-    See :doc:`/gallery/misc/date_precision_and_epochs` for a discussion.
+    See :doc:`/gallery/ticks_and_spines/date_precision_and_epochs` for a
+    discussion.
 
     Parameters
     ----------
